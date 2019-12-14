35ac3af5342ef2d3dff77c68bc4932fad73270f7
matthew-nicholas-smith <56278361+matthew-nicholas-smith@users.noreply.github.com>
2019-12-14 14:42:34 -0500

    Update characters.js

--- dontdoubleclick/characters.js
+++ dontdoubleclick/characters.js
@@ -1,3 +1,7 @@
+var sprites = [];
+function newSprite(){
+  sprites.push(sprite = new THREE.Mesh( geometry, material ));
+}
 geometry = new THREE.BoxGeometry( 1, 1, 1 );
 material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 sprite = new THREE.Mesh( geometry, material );
